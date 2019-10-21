4e30800865cda77e545bb2135ba6e318bf074893
Naimesh Chaudhari <nchaudh032gmail.com>
2019-10-21 14:19:49 -0500

    added utility folder for kangs functions that get the books and chapters

--- FlaskApp/app.py
+++ FlaskApp/app.py
@@ -1,5 +1,6 @@
 from flask import Flask
 from flask import Flask, render_template
+from utils.getData import getBooks, getChapters
 
 app = Flask(__name__)
 
@@ -8,8 +9,7 @@ app = Flask(__name__)
 @app.route('/index')
 def home():
     #Get all boooks avaliable
-    random_list = ['Book1', 'Book2', 'Book3', 'Book4','Book5','Book6','Book7','Book8']
-    return render_template("home.html", data = random_list)
+    return render_template("home.html", data = getBooks())
 
 @app.route("/about")
 def about():
@@ -18,12 +18,7 @@ def about():
 @app.route("/chapterSelection/<string:book>")
 def chapterSelection(book):
     #Get Chapter Information for selected book
-    chapters = {'Chapter1' : ['Section 1', 'Section 2', 'Section 3 ', 'Section 4','Section 5']
-              , 'Chapter2' : ['Section 1', 'Section 2', 'Section 3 ']
-              ,'Chapter3' : ['Section 1', 'Section 2']
-              ,'Chapter4' : ['Section 1', 'Section 2', 'Section 3 ', 'Section 4','Section 5','Section 6']
-                }
-    return render_template("ChapterSelection.html", data = chapters)
+    return render_template("ChapterSelection.html", data = getChapters(book))
 
 @app.route("/GenYaml")
 def GenYaml():
