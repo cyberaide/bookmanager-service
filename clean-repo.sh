0f77f3406abe08b9ef5151d11f84c67a417bac16
Gregor von Laszewski <laszewski@gmail.com>
2019-12-18 09:57:19 -0500

    clean repo script

---
+++ clean-repo.sh
@@ -0,0 +1,30 @@
+gitclean()
+{
+    git filter-branch --force --index-filter "git rm --cached --ignore-unmatch $1" --prune-empty --tag-name-filter cat -- --all
+}
+
+#gitclean books/site/404.html
+
+gitclean books/site/404.html
+gitclean books/site/about/index.html
+gitclean books/site/css/theme.css
+gitclean books/site/css/theme_extra.css
+gitclean books/site/fonts/fontawesome-webfont.eot
+gitclean books/site/fonts/fontawesome-webfont.svg
+gitclean books/site/fonts/fontawesome-webfont.ttf
+gitclean books/site/fonts/fontawesome-webfont.woff
+gitclean books/site/img/favicon.ico
+gitclean books/site/index.html
+gitclean books/site/js/jquery-2.1.1.min.js
+gitclean books/site/js/modernizr-2.8.3.min.js
+gitclean books/site/js/theme.js
+gitclean books/site/search.html
+gitclean books/site/search/lunr.js
+gitclean books/site/search/main.js
+gitclean books/site/search/search_index.json
+gitclean books/site/search/worker.js
+gitclean books/site/sitemap.xml
+gitclean books/site/sitemap.xml.gz
+
+#git push origin --force --all
+#git push origin --force --tags
