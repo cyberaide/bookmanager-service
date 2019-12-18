25df49c8998bbdaaf883db65a2c349cb14fc95b7
Gregor von Laszewski <laszewski@gmail.com>
2019-12-18 11:15:17 -0500

    remove files form git

--- clean-repo.sh
+++ clean-repo.sh
@@ -3,41 +3,41 @@ gitclean()
     git filter-branch --force --index-filter "git rm --cached --ignore-unmatch $1" --prune-empty --tag-name-filter cat -- --all
 }
 
-#gitclean books/site/404.html
+gitclean books/site/404.html
 
-# gitclean books/site/404.html
-# gitclean books/site/about/index.html
-# gitclean books/site/css/theme.css
-# gitclean books/site/css/theme_extra.css
-# gitclean books/site/fonts/fontawesome-webfont.eot
-# gitclean books/site/fonts/fontawesome-webfont.svg
-# gitclean books/site/fonts/fontawesome-webfont.ttf
-# gitclean books/site/fonts/fontawesome-webfont.woff
-# gitclean books/site/img/favicon.ico
-# gitclean books/site/index.html
-# gitclean books/site/js/jquery-2.1.1.min.js
-# gitclean books/site/js/modernizr-2.8.3.min.js
-# gitclean books/site/js/theme.js
-# gitclean books/site/search.html
-# gitclean books/site/search/lunr.js
-# gitclean books/site/search/main.js
-# gitclean books/site/search/search_index.json
-# gitclean books/site/search/worker.js
-# gitclean books/site/sitemap.xml
-# gitclean books/site/sitemap.xml.gz
+gitclean books/site/404.html
+gitclean books/site/about/index.html
+gitclean books/site/css/theme.css
+gitclean books/site/css/theme_extra.css
+gitclean books/site/fonts/fontawesome-webfont.eot
+gitclean books/site/fonts/fontawesome-webfont.svg
+gitclean books/site/fonts/fontawesome-webfont.ttf
+gitclean books/site/fonts/fontawesome-webfont.woff
+gitclean books/site/img/favicon.ico
+gitclean books/site/index.html
+gitclean books/site/js/jquery-2.1.1.min.js
+gitclean books/site/js/modernizr-2.8.3.min.js
+gitclean books/site/js/theme.js
+gitclean books/site/search.html
+gitclean books/site/search/lunr.js
+gitclean books/site/search/main.js
+gitclean books/site/search/search_index.json
+gitclean books/site/search/worker.js
+gitclean books/site/sitemap.xml
+gitclean books/site/sitemap.xml.gz
 
-# gitclean site/Dockerfile
-# gitclean site/README.md
-# gitclean site/VERSION
-# gitclean site/generateYAML.pynb
-# gitclean site/index.md
-# gitclean site/manual.md
-# gitclean site/requirements.txt
-# gitclean site/sample.jpg
-# gitclean site/setup.cfg
-# gitclean site/setup.py
-# gitclean site
-# gitclean site1
+gitclean site/Dockerfile
+gitclean site/README.md
+gitclean site/VERSION
+gitclean site/generateYAML.pynb
+gitclean site/index.md
+gitclean site/manual.md
+gitclean site/requirements.txt
+gitclean site/sample.jpg
+gitclean site/setup.cfg
+gitclean site/setup.py
+gitclean site
+gitclean site1
 
 gitclean docusaurustemplate/404.html
 gitclean docusaurustemplate/about/index.html
@@ -151,5 +151,5 @@ gitclean dest/c0bfc72630636e6b02a9fb328c38a62a.epub
 gitclean dest/cbff23a6ccf84db25c29d67868508208.epub
 gitclean dest
 
-#git push origin --force --all
-#git push origin --force --tags
+git push origin --force --all
+git push origin --force --tags
