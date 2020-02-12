610d107c5eea9bbfb05dc0ae849e2212ea665796
Gregor von Laszewski <laszewski@gmail.com>
2020-02-12 14:08:29 -0500

    remove comment

--- cloudmesh/bookmanagerservice/bmservice.py
+++ cloudmesh/bookmanagerservice/bmservice.py
@@ -1,13 +1,6 @@
 import subprocess
 from pathlib import Path
 
-#
-# sshoudl that not be integrated into
-#
-# bbookmanagerserice.command.bookmanagerservice.
-# than we can use cms bookmanagerservice ....?
-#
-
 host = "0.0.0.0"
 #host = "127.0.0.1"
 port = "5000"
@@ -18,7 +11,7 @@ def bmservice():
     directory = str(Path('.').absolute())
     strt = f'docker run --rm -it -p {host}:{port}:{port}/tcp -w /opt/project/bookmanager-service '
     envs = '-e "FLASK_APP=/opt/project/bookmanager-service/cloudmesh/bookmanagerservice/service/app.py" -e "FLASK_ENV=development" '
-    vol = f'-v {directory}:/opt/project/bookmanager-service {image}:v1 '
+    vol = f'-v {directory}:/opt/project/bookmanager-service {image}:latest '
     pcmd = f'python -u -m flask run --host {host}'
     final = strt + envs + vol + pcmd
     print(final)
